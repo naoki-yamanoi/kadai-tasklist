2ef006d8c95101c1e1e90297368ae87ffb011eff
naoki <ti44nnyy999@gmail.com>
2020-09-14 18:47:02 +0900

    Add DestroyServlet.java

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -32,18 +32,17 @@ public class EditServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        接続
         EntityManager em = DBUtil.createEntityManager();
-//        編集したいidに一致するタスクを取得
+
         Task t = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
-//        切断
+
         em.close();
-//        タスクとセッションIDをリクエストスコープに保管
+
         request.setAttribute("task", t);
         request.setAttribute("_token", request.getSession().getId());
-//        タスクIDをセッションスコープに保管
+
         request.getSession().setAttribute("task_id", t.getId());
-//        JSPへフォワード
+
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
     }
